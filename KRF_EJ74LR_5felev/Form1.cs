0803d9e057725e98b887dbf22bd6f0e1cbbc451c
Ferenc.Kurucz <Ferenc.Kurucz@W-HU1-KFE>
2017-01-11 08:15:05 +0100

    Add project files.

---
+++ KRF_EJ74LR_5felev/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KRF_EJ74LR_5felev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
